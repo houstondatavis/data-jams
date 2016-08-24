68a8dd420b53370ec33246fe97061c0076bac0e7
pandafulmanda <pandafulmanda@gmail.com>
2016-08-24 10:44:54 -0500

    paragraph text was hard to read without proper caps

--- data/events.js
+++ data/events.js
@@ -1,13 +1,13 @@
-var description = "we work in small teams to visualize a new data set in three hours. we announce the actual data set at the beginning of the event. all jams are tool agnostic, so you can choose to use what you're already comfortable with or try something new.\n\n\
-the goal is to learn from each other by producing many visualizations on a common data set; we'll often identify interesting patterns, but more importantly we get to quickly compare design choices, statistical methods, tool choices, failures and successes. \n\n\
-approximate schedule:\n\n\
+var description = "We work in small teams to visualize a new data set in three hours. We announce the actual data set at the beginning of the event. All jams are tool agnostic, so you can choose to use what you're already comfortable with or try something new.\n\n\
+The goal is to learn from each other by producing many visualizations on a common data set; we'll often identify interesting patterns, but more importantly we get to quickly compare design choices, statistical methods, tool choices, failures and successes. \n\n\
+Approximate schedule:\n\n\
 * 9a coffee & bagels\n\
-  * data set announced\n\
-  * form teams, ready your laptops\n\
-* 930a visualize it\n\
-* 1230p quick talks to share the visualizations we created";
+  * Data set announced\n\
+  * Form teams, ready your laptops\n\
+* 930a Vsualize it\n\
+* 1230p Quick talks to share the visualizations we created";
 
-var shareInstructions = "\nthis is all about learning, so please share even if it's not polished :)";
+var shareInstructions = "\nThis is all about learning, so please share even if it's not polished :)";
 
 var baseJam = {
   topic: '',
