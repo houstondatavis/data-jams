45aec471b992442ff03daca40f6d738f6425032c
pandafulmanda <pandafulmanda@gmail.com>
2016-08-24 00:44:24 -0500

    try to be more consistent about data set vs dataset.  also, make the pun more punny

--- data/events.js
+++ data/events.js
@@ -1,9 +1,8 @@
-var description = "we work in small teams to visualize a new dataset in three hours. we announce the actual dataset at the beginning of the event. all jams are tool agnostic, so you can choose to use what you're already comfortable with or try something new.\n\n\
-the goal is to learn together and from each other by producing many visualizations on a common dataset. we get to quickly compare design choices, statical methods, tool choices, failures, successes, interesting patterns...really anything about the process! the control is the base dataset, and everything else is up to you.\n\n\
-bonus points for the enterprising souls who make something you can view and share on the web. \n\n\
+var description = "we work in small teams to visualize a new data set in three hours. we announce the actual data set at the beginning of the event. all jams are tool agnostic, so you can choose to use what you're already comfortable with or try something new.\n\n\
+the goal is to learn together and from each other by producing many visualizations on a common data set. we get to quickly compare design choices, statical methods, tool choices, failures, successes, interesting patterns...really anything about the process! the control is the base data set, and everything else is variable and up to you.\n\n\
 approximate schedule:\n\n\
 * 9a coffee & bagels\n\
-  * dataset announced\n\
+  * data set announced\n\
   * form teams, ready your laptops\n\
 * 930a visualize it\n\
 * 1230p quick talks to share the visualizations we created";
