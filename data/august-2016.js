1fb9235af518cb82b58098f4a12eebde50d7d42b
neerajt <neerajtandon25@gmail.com>
2016-08-24 09:40:51 -0500

    Update august-2016.js
    Expands the description

--- data/august-2016.js
+++ data/august-2016.js
@@ -5,6 +5,9 @@ var augustEvent = {
   formUrl: 'https://goo.gl/forms/GNawy82uRoQt5zbv1',
   month: 'august',
   year: '2016',
-  description: "The data includes primary results and demographics by county.\
-  During the data jam, the data set was enriched with primary schedule as well."
+  description: "The data includes US primary election results from 2016 and demographics by county.\n\
+  Vote data included variables like `Vote_count` per candidate. County data included age and racial \
+  breakdown, educational attainment, employment etc., as well as shapefiles for the counties.\n\
+  Data were sourced from a [Kaggle competition](https://www.kaggle.com/benhamner/2016-us-election.\n\
+  and were enriched with primary schedule, and primary type (open, closed, etc.) during the data jam."
 };
