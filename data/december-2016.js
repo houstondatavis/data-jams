a41ed6680714efedbe50edca787f89b5dccb38ce
pandafulmanda <pandafulmanda@gmail.com>
2016-12-04 10:08:34 -0600

    adding victor and randy showcases

--- data/december-2016.js
+++ data/december-2016.js
@@ -14,11 +14,42 @@ var december2016Event = {
       url: 'https://public.tableau.com/profile/justin.gosses#!/vizhome/HoustonNorthsideNeighborhoodCrimeExploration2016_versionB/NorthsideHoustonCrime'
     }]
   }, {
+    title: 'In depth analyis in R',
+    author: 'Victor',
+    description: "",
+    links: [{
+      type: ['plot', 'code'],
+      url: 'https://houstondatavis.github.io/data-jam-december-2016/victor-analysis/'
+    }]
+  }, {
     title: 'Offense by time',
     author: 'Andy',
+    description: "",
     links: [{
       type: ['plot'],
       url: 'https://public.tableau.com/profile/publish/DataJam12032016/DataJam12032016#!/publish-confirm'
+    }]
+  }, {
+    title: 'Crimes on a map',
+    author: 'Randy',
+    description: "",
+    links: [{
+      type: ['plot'],
+      url: 'https://randy7771026.github.io/jcrime/'
+    }, {
+      type: ['code'],
+      url: 'https://github.com/randy7771026/jcrime/tree/gh-pages'
+    }]
+  }, {
+    title: 'Map with timelapse',
+    author: 'Randy',
+    description: "Same map as above, with quick timelapse",
+    links: [{
+      type: ['plot'],
+      url: 'https://pandafulmanda.github.io/jcrime/'
+    }, {
+      type: ['code'],
+      url: 'https://github.com/pandafulmanda/jcrime/tree/gh-pages'
     }]
   }]
 };
