3245927c9bd67e6a2ef76ce8f24936e89bb3c832
pandafulmanda <pandafulmanda@gmail.com>
2017-02-25 16:15:58 -0600

    add feb 2017 hobby jam

---
+++ data/february-2017.js
@@ -0,0 +1,10 @@
+var feb2017Event = {
+  topic: 'Houston Hobby Airport data',
+  meetupId: '237127703',
+  githubRepo: 'data-jam-february-2017',
+  formUrl: 'https://goo.gl/forms/csXnKYIVaJFDdYv93',
+  month: 'February',
+  year: '2017',
+  description: "For this month's data jam, we will be working with data from Monday at the Houston Hobby airport. This data is sourced from FlightAware's API.",
+  showcase: []
+};
