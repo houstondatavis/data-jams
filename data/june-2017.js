6d3e1add2f72306d73b3c771cc4d94b6b63cfebd
amanda <pandafulmanda@gmail.com>
2017-06-23 22:49:11 -0500

    add june stuffs

---
+++ data/june-2017.js
@@ -0,0 +1,9 @@
+var june2017Event = {
+  topic: 'Traffic stop data from Texas',
+  meetupId: '240580139',
+  githubRepo: 'data-jam-june-2017',
+  formUrl: 'https://goo.gl/forms/ypiOz25QSrHCbrzy2',
+  month: 'June',
+  year: '2017',
+  description: "From [The Stanford Open Policing Project](https://openpolicing.stanford.edu/)."
+};
