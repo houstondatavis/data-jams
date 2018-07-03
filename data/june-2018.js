cd23035b742ad2278b1207c3dee6d836e25cf0bf
amanda <pandafulmanda@gmail.com>
2018-07-02 23:03:05 -0500

    add first june showcase link

--- data/june-2018.js
+++ data/june-2018.js
@@ -5,5 +5,13 @@ var june2018Event = {
   formUrl: 'https://goo.gl/forms/H39YAXzVhNWAz6Kg2',
   month: 'June',
   year: '2018',
-  description: "\"In cooperation with the City of Houston, the U.S. Geological Survey collects, processes, and interprets water-quality, streamflow, and reservoir water elevation and capacity data. These data are collected to better understand the effects of urbanization in Lake Houston, a drinking water reservoir in Houston, Texas. The combined monitoring efforts of Lake Houston and the associated watershed increase the understanding of the effects of natural and controlled inflows, nutrient and sediment loads, and water-column structure on the chemistry and biology of Lake Houston.\" -- [Water-Quality Monitoring on Lake Houston from USGS](https://webapps.usgs.gov/lake_houston/home/#realtime)"
+  description: "\"In cooperation with the City of Houston, the U.S. Geological Survey collects, processes, and interprets water-quality, streamflow, and reservoir water elevation and capacity data. These data are collected to better understand the effects of urbanization in Lake Houston, a drinking water reservoir in Houston, Texas. The combined monitoring efforts of Lake Houston and the associated watershed increase the understanding of the effects of natural and controlled inflows, nutrient and sediment loads, and water-column structure on the chemistry and biology of Lake Houston.\" -- [Water-Quality Monitoring on Lake Houston from USGS](https://webapps.usgs.gov/lake_houston/home/#realtime)",
+  showcase: [{
+  title: 'A Map!',
+  author: 'Justin Gosses',
+  description: "A basic map. Trying [Kepler.gl](https://uber.github.io/kepler.gl/) for map making using watershed geojson I already had, some FEMA modeled damage data points in geojson, and quick geojson of main water quality sampling stations. I wanted a map that I could easily change the basemap and add other features to without a lot of work.",
+  links:[{
+    type: ['code', 'plot'],
+    url: 'https://github.com/houstondatavis/data-jam-June-2018/blob/master/pre_made_kepler_map/instructions_for_kepler_map.md'
+  }]
 };
